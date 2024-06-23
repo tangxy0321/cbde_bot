92ab11722136bb7ed3af359de21b96c2eb95a6ac
tangxy0321 <xinyi.tang.2020@smu.edu.sg>
2024-06-23 20:23:13 +0800

    streamline transition between challenges

--- app/bot.py
+++ app/bot.py
@@ -9,7 +9,7 @@ BOT_TOKEN = os.getenv("BOT_TOKEN")
 
 bot = telebot.TeleBot(BOT_TOKEN)
 
-
+challenge2_quiz = []
     
 @bot.message_handler(commands=['start'])
 def send_welc(msg):
@@ -50,19 +50,24 @@ def dance_quiz(query):
         explanation=forfeit1_text,
         explanation_entities=[forfeit1],
     )
-    
-    time.sleep(25)
 
-    # challenge 2
-    reply_chall2 = telebot.types.InlineKeyboardMarkup(
-        [
-            [telebot.types.InlineKeyboardButton(text="Start!",callback_data="image_quiz")]
-        ]
-    )
+    @bot.poll_answer_handler(lambda poll: poll.poll_id == dance_quiz.poll.id)
+    def handle_poll(poll):
+        if len(poll.option_ids) == 1:
+            bot.stop_poll(chat_id=query.message.chat.id,message_id=dance_quiz.message_id)
 
-    chall2 = bot.send_message(chat_id=query.message.chat.id, text="\U0001F624Just a dance is not enough to show how much you love them \nYou have to prove your love by recognising... \n their BODY!!!",
-        reply_markup=reply_chall2
-    )
+            time.sleep(3)
+  
+            # challenge 2
+            reply_chall2 = telebot.types.InlineKeyboardMarkup(
+                [
+                    [telebot.types.InlineKeyboardButton(text="Start!",callback_data="image_quiz")]
+                ]
+            )
+
+            chall2 = bot.send_message(chat_id=query.message.chat.id, text="\U0001F624Just a dance is not enough to show how much you love them \nYou have to prove your love by recognising... \n their BODY!!!",
+                reply_markup=reply_chall2
+            )
 
 @bot.callback_query_handler(lambda query: query.data == "image_quiz")
 def image_quiz(query):
@@ -123,7 +128,7 @@ def image_quiz(query):
 
     # candidate 3
     image3 = bot.send_photo(chat_id=query.message.chat.id,photo=open("./app/photo/hanbin.jpg","rb"),caption="This is our candidate 3")
-    image3_quiz = bot.send_poll(
+    image3_quiz3 = bot.send_poll(
         chat_id=query.message.chat.id,
         question="Guess whose this is:",
         options=[
@@ -147,37 +152,58 @@ def image_quiz(query):
         
     )
 
-    time.sleep(55)
 
-    # clue: challenge 2
-    bot.send_message(chat_id=query.message.chat.id, text="Here's your clue for challenge 2~ \nGuess the consonant key: \nThey are all 남성/남자.")
+    @bot.poll_answer_handler(lambda pollans: pollans.poll_id in [image1_quiz.json["poll"]["id"], image2_quiz.json["poll"]["id"], image3_quiz3.json["poll"]["id"]] )
+    def handle_challenge2_polls(poll):
 
-    time.sleep(20)
+        quiz1 = image1_quiz
+        quiz2 = image2_quiz
+        quiz3 = image3_quiz3
 
-    # challenge 3
-    forfeit3_text = "Forfeit time!\nHOW COULD YOU \nYou got no clue for this round!"
-    forfeit3 = telebot.types.MessageEntity(type="bold",offset=0,length=len(forfeit3_text))
+        if len(poll.option_ids) > 0:
+            if poll.poll_id == quiz1.json["poll"]["id"]:
+                image1_quiz_closed = bot.stop_poll(chat_id=query.message.chat.id, message_id=quiz1.message_id)
+                challenge2_quiz.append(image1_quiz_closed)
+            elif poll.poll_id == quiz2.json["poll"]["id"]:
+                image2_quiz_closed = bot.stop_poll(chat_id=query.message.chat.id, message_id=quiz2.message_id)
+                challenge2_quiz.append(image2_quiz_closed)
+            elif poll.poll_id == quiz3.json["poll"]["id"]:
+                image3_quiz_closed = bot.stop_poll(chat_id=query.message.chat.id, message_id=quiz3.message_id)
+                challenge2_quiz.append(image3_quiz_closed)
 
-    bot.send_message(chat_id=query.message.chat.id,text="You're at the last step before treasure location reveal... \nHere comes your last clue!")
-    image3_quiz = bot.send_poll(
-        chat_id=query.message.chat.id,
-        question="Who's more important:",
-        options=[
-            telebot.types.InputPollOption("Us"),
-            telebot.types.InputPollOption("Sung Hanbin")
-        ],
-        type="quiz",
-        allows_multiple_answers=False,
-        correct_option_id=0,
-        is_anonymous=False,
-        open_period=3,
-        explanation=forfeit3_text,
-        explanation_entities=[forfeit3],
-        
-    )
+        if len(challenge2_quiz) == 3:
+            if (challenge2_quiz[0].is_closed == True) and (challenge2_quiz[1].is_closed == True) and (challenge2_quiz[2].is_closed == True):         
 
-    time.sleep(3)
-    bot.send_message(chat_id=query.message.chat.id,text="Time to guess who your treasure is with~")
+                time.sleep(2)
+
+                # clue: challenge 2
+                bot.send_message(chat_id=query.message.chat.id, text="Here's your clue for challenge 2~ \nGuess the consonant key: \nThey are all 남성/남자.")
+
+                time.sleep(5)
+
+                # challenge 3
+                forfeit3_text = "Forfeit time!\nHOW COULD YOU \nYou got no clue for this round!"
+                forfeit3 = telebot.types.MessageEntity(type="bold",offset=0,length=len(forfeit3_text))
+
+                bot.send_message(chat_id=query.message.chat.id,text="You're at the last step before treasure location reveal... \nHere comes your last clue!")
+                image3_quiz = bot.send_poll(
+                    chat_id=query.message.chat.id,
+                    question="Who's more important:",
+                    options=[
+                        telebot.types.InputPollOption("Us"),
+                        telebot.types.InputPollOption("Sung Hanbin")
+                    ],
+                    type="quiz",
+                    allows_multiple_answers=False,
+                    correct_option_id=0,
+                    is_anonymous=False,
+                    open_period=3,
+                    explanation=forfeit3_text,
+                    explanation_entities=[forfeit3],
+                    
+                )
+                time.sleep(2)
+                bot.send_message(chat_id=query.message.chat.id,text="Time to guess who your treasure is with~")
 
 
 
